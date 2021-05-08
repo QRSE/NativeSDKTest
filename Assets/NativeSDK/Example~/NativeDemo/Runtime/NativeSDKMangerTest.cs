2b4e04b31999520156be75d144848abfb870d720
QRSE <12883293@qq.com>
2021-05-08 10:49:16 +0800

    Add Test Demo

---
+++ Assets/NativeSDK/Example~/NativeDemo/Runtime/NativeSDKMangerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Native.SDK;
+using UnityEngine;
+
+public class NativeSDKMangerTest : MonoBehaviour
+{
+	public void Awake()
+	{
+		SpeechToText.Init();
+		TextToSpeech.Init();
+	}
+}
